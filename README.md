89f658b4cd744ca833177eb6a7c5bfd2b5755020
Aveesh51 <aveeshshetty120@gmail.com>
2019-08-20 10:46:19 +0530

    purchase summary

---
+++ README.md
@@ -0,0 +1,16 @@
+![Alt text](https://monosnap.com/image/yTLnJcL4OKpoC1X7cnZWzrdSaRjGLO)
+![Alt text](https://monosnap.com/image/irShMiVk2rWjAHKLU2y93AsgGoiDUi)
+![Alt text](https://monosnap.com/image/vCgVYVyax2EO139QWbs3dapyySAIHC)
+
+# Tools
+* create-react-app cli
+* react-bootstrap
+* react-redux
+# Start App
+
+Install CRA globally, Clone repo, install, cd into folder and run:
+```git
+npm install -g create-react-app
+npm install
+npm start
+```
---
+++ src/components/ItemDetails/ItemDetails.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Button, Collapse, Well, Media, Row, Col } from 'react-bootstrap';
+export default class ItemDetails extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      open: false
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Button
+          className="item-details-button"
+          bsStyle="link"
+          onClick={() => this.setState({ open: !this.state.open })}
+        >
+          {this.state.open === false ? `See` : `Hide`} item details{' '}
+          {this.state.open === false ? `+` : `-`}
+        </Button>
+        <Collapse in={this.state.open}>
+          <div>
+            <Well>
+              <Media>
+                <Media.Left>
+                  <img
+                    width={100}
+                    height={100}
+                    src="https://i5.walmartimages.com/asr/e73e1252-642c-4473-93ea-fd3b564a7027_1.3e81ea58fa3042452fe185129a4a865f.jpeg?odnWidth=undefined&odnHeight=undefined&odnBg=ffffff"
+                    alt="thumbnail"
+                  />
+                </Media.Left>
+                <Media.Body>
+                  <p>
+                    Essentials by OFM ESS-3085 Racing Style Leather Gaming
+                    Chair, Red
+                  </p>
+                  <Row className="show-grid">
+                    <Col md={6}>
+                      <strong>{`$${this.props.price}`}</strong>
+                      <br />
+                      <strong className="price-strike">{`$${
+                        this.props.price
+                      }`}</strong>
+                    </Col>
+                    <Col md={6}>
+                      <strong>Qty: 1</strong>
+                    </Col>
+                  </Row>
+                </Media.Body>
+              </Media>
+            </Well>
+          </div>
+        </Collapse>
+      </div>
+    );
+  }
+}
